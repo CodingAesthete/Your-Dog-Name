e41618b47772a4e81dbcf8978cdb0fef322fbe2c
CodingAesthete <harry_olli@abv.bg>
2023-11-30 10:55:36 +0200

    Add dog's nationality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <small className="info-code">2023 Entertaiment Colors Page</small>
+      <small className="info-code">2023 Entertaiment Dogs Page</small>
     </div>
   )
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,29 +2,49 @@ import React from "react";
 
 function MainContent() {
 
-  const [color, setColor] = React.useState({
+  const [dog, setDog] = React.useState({
     topText: "",
     bottomText: "",
-    url: "https://s7d1.scene7.com/is/image/terrain/24781320_018_e?$zoom2$"
+    url: "https://s7d1.scene7.com/is/image/terrain/24781320_018_e?$zoom2$",
+    flag: "https://static.thenounproject.com/png/3191538-200.png"
+  })
+
+  const [countries, setCountries] = React.useState([]);
+
+  React.useEffect(function () {
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => setCountries(data))
   })
 
   function handleChange(e) {
     const { name, value } = e.target;
-    setColor(function () {
+    setDog(function () {
       return {
-        ...color,
+        ...dog,
         [name]: value
       }
     })
   }
 
-  function invokeColor() {
+  function invokeDog() {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((res) => res.json())
-      .then((data) => setColor((prevColor) => ({
+      .then((data) => setDog((prevColor) => ({
         ...prevColor,
         url: data.message
       })));
+  }
+
+  function invokeCountry() {
+    const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+    const countryFlag = randomCountry.flags.png;
+    setDog(function () {
+      return {
+        ...dog,
+        flag: countryFlag
+      }
+    })
   }
 
   return (
@@ -34,22 +54,24 @@ function MainContent() {
           type="text"
           name="topText"
           placeholder="Type your first name"
-          value={color.topText}
+          value={dog.topText}
           onChange={handleChange} />
 
         <input
           type="text"
           name="bottomText"
           placeholder="Type the name of the dog's breed + -ov/ev/-sk"
-          value={color.bottomText}
+          value={dog.bottomText}
           onChange={handleChange} />
 
-        <button onClick={invokeColor}>Choose new color</button>
+        <button onClick={invokeDog}>Choose new dog</button>
       </div>
       <div className="img-cont">
-        <img src={color.url} />
-        <h3 className="top-text content-text">{color.topText}</h3>
-        <h3 className="bottom-text content-text">{color.bottomText}</h3>
+        <img onClick={invokeCountry} className="main-img" src={dog.url} />
+        <img className="flag-img left" src={dog.flag} />
+        <img className="flag-img right" src={dog.flag} />
+        <h3 className="top-text content-text">{dog.topText}</h3>
+        <h3 className="bottom-text content-text">{dog.bottomText}</h3>
       </div>
     </main>
   )
